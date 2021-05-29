4a2c2971de9c5bb1b2abfcea7d4c0513cc576668
Facundo Agustín Fernández <facuassain@gmail.com>
2021-05-29 20:57:33 -0300

    README updated

---
+++ README.md
@@ -0,0 +1,3 @@
+## JavaScript Algo
+
+**Write a function that logs the 5 cities that occur the most in the array below in order from the most number of occurrences to least.**
--- logMostOccur.js
+++ logMostOccur.js
@@ -1,7 +1,3 @@
-/**
-    Write a function that logs the 5 cities that occur the most in the array below in
-    order from the most number of occurrences to least.
-*/
 
 const citiesList = [
     "nashville",
